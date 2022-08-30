da46e376cd001e652779e334d9341779615bbc7e
Timetraveler06 <101510934+Timetraveler06@users.noreply.github.com>
2022-08-30 17:27:14 +0300

    Login page connected with data base

--- Inventory_Control/Inventory/Inventory/DBService.cs
+++ Inventory_Control/Inventory/Inventory/DBService.cs
@@ -9,5 +9,21 @@ namespace Inventory
 {
     internal class DBService
     {
+        string path = "Data Source= .;InitialCatalog= student;Intergrated security=false;";
+        SqlConnection _connection;
+        public DBService()
+        {
+            try
+            {
+                _connection = new SqlConnection(path);
+                _connection.Open();
+               
+
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
--- Inventory_Control/Inventory/Inventory/Form1.cs
+++ Inventory_Control/Inventory/Inventory/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Inventory
@@ -19,18 +20,51 @@ namespace Inventory
 
         private void Login_Load(object sender, EventArgs e)
         {
+           
+            
 
         }
 
         private void btnlogin_Click(object sender, EventArgs e)
 
         {
-            Login form = new Login();
-            DataValidation dv;
-            MainContainer mainContainer = new MainContainer();
-            string username = txtusername.Text;
-            string password = txtpassword.Text;
-            dv= new DataValidation(username,password);
+            string path = "Data Source= DESKTOP-02T8VQI\\SQLEXPRESS;Initial Catalog= Inventory;Integrated security=true;";
+            SqlConnection conn;
+            try
+            {
+                Login form = new Login();
+                DataValidation dv;
+                MainContainer mainContainer = new MainContainer();
+                string username = txtusername.Text;
+                string password = txtpassword.Text;
+                SqlCommand cmd;
+
+                dv = new DataValidation(username, password);
+                
+                if (dv.ValidateLogin())
+                {
+                    conn = new SqlConnection(path);
+                    conn.Open();
+                    cmd = new SqlCommand("INSERT INTO LoginPage VALUES('" + username + "','" + password + "')", conn);
+                  
+                    cmd.ExecuteNonQuery();
+                    mainContainer.Show();
+                    form.Hide();
+
+                }
+               
+               
+
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+           // dv= new DataValidation(username,password);
+            /*
             if (dv.ValidateLogin())
             {
                 mainContainer.Show();
@@ -39,7 +73,7 @@ namespace Inventory
             }
             else
                 MessageBox.Show(" Incorrect UserName or Password");
-
+            */
 
         }
 
